6eaa7c4a3c115fd12c98a2d816049841f65e4527
Abdulmalik Aremu <aremzy2018@gmail.com>
2025-09-29 16:26:37 +0100

    just some minor migration fixes in the django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """
+    Admin configuration for CustomUser model.
+    
+    Extends Django's UserAdmin to work with our custom user model
+    that uses UUID primary key and email as username.
+    """
+    
+    # Fields to display in the admin list view
+    list_display = ('email', 'full_name', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'date_joined')
+    search_fields = ('email', 'full_name', 'username')
+    ordering = ('email',)
+    
+    # Fields to display when editing a user
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'full_name', 'first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Fields to display when adding a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2'),
+        }),
+    )
+    
+    # Make email read-only in the admin since it's the username field
+    readonly_fields = ('date_joined', 'last_login')
